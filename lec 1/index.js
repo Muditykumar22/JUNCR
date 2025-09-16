bb61f80fa6a93f2a89cb2056067138b883b178cc
Muditykumar22 <mudity.kumar_cs22@gla.ac.in>
2025-09-16 19:59:25 +0530

    lec-1

---
+++ lec 1/index.js
@@ -0,0 +1,46 @@
+// // const y=30;
+// // const x=10;
+// // x=20
+// // {
+// //     console.log("ravinder");
+// //     {
+// //         console.log(x*x);
+// //         let y = 40;
+// //         console.log(y);
+// //         y=50;
+
+// //     }
+// //     console.log(y)
+// // }
+
+// let x=89;
+// {
+//   console.log(x);
+//   x=90;
+// }
+// console.log(x);
+// x=91;
+// console.log(x);
+    
+// console.log(typeof(x));
+
+// // console.log(z);
+// let k="undefined";
+// console.log(k);
+// console.log(typeof(k));
+// let arr=[23,56,3,7,"hero",true,null,undefined,34.67,23,56];
+// console.log(arr);
+// for(let i = 0;i<arr.length;i++){
+//     console.log(arr[i]);
+// }
+// console.log(typeof(arr));
+let x = {
+    "name": "ashish ",
+    "age": 19,
+    "mark": 100,
+    "prsent": true,
+    "name":"shaurabh"
+    
+}
+console.log(x);
+console.log(typeof (x));
