5829c3032ac3f947c3a7d8695d89bc5ab99c0cf1
Muditykumar22 <mudity.kumar_cs22@gla.ac.in>
2025-09-17 18:24:00 +0530

    updated

---
+++ lec 2/hello.js
@@ -0,0 +1,32 @@
+// let x ={
+//     "full name": "John",
+//     id: 12345,
+//     age: 20,
+//     city: "New York",
+//     country: "USA",
+//     email: "john@gmail.com",
+//     phone: "1234567890",
+//     address: {
+//         street: "123 Main St",
+//         city: "New York",
+//         state: "NY",
+//         zip: "10001",
+//         country: "USA"
+//     },
+// }
+// console.log(x);
+// x["age"] = 21;
+// delete x["age"];
+var x = 90;
+        {let x=94;
+            console.log(x);
+            {
+                console.log(x);
+                x = 91;
+                console.log(x);
+
+
+            }
+            console.log(x);
+        }
+        console.log(x);
